00ad3329a43fd94a825c472f796aa90605b3403e
DoanPhuc95 <phucdm1901@gmail.com>
2017-06-27 00:42:33 +0700

    chap 12

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,10 +4,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = t ".flash.success"
+      flash[:success] = t "activate_success"
       redirect_to user
     else
-      flash[:danger] = t ".flash.danger"
+      flash[:danger] = t "flahs_danger"
       redirect_to root_url
     end
   end
